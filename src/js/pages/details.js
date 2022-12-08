41cdba33087579ee1aceb9169ce1be867fca8604
WParsec <91538926+WParsec@users.noreply.github.com>
2022-12-08 22:09:53 +0100

    completed fetch and build of details page

--- src/js/pages/details.js
+++ src/js/pages/details.js
@@ -1,18 +1,24 @@
 import { hideShowLi } from "../ui/hideShowLi.js";
 import { toggleMenu } from "../ui/nav/toggleMenu.js";
 import { getCreditAmount } from "../ui/getCreditAmount.js";
+import { standardFetch } from "../fetch/fetch.js";
+import { createStandardHeader } from "../headers/headers.js";
+import { baseUrl, allListingsUrl, sellerFlag } from "../data/constants.js";
+import { formatDate } from "../utils/formatDate.js";
 
 // Initiate
 hideShowLi();
 toggleMenu();
 getCreditAmount();
 
+// Initiate variables
+let count = 0;
+let arrayLength;
+
 // Collecting ID from querystring
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-
-console.log(id);
 
 // Remember to change name of title of page
 
@@ -21,13 +27,64 @@ const sliderContainer = document.querySelector("#sliderContainer");
 const mediaNext = document.querySelector("#mediaNext");
 const mediaBack = document.querySelector("#mediaBack");
 
-let count = 0;
-let arrayLength = 2;
+async function buildDetailsPage(id) {
+  const response = await standardFetch(baseUrl + allListingsUrl + "/" + id + sellerFlag, createStandardHeader());
+  const {
+    description,
+    endsAt,
+    bids,
+    media,
+    tags,
+    title,
+    seller: { name },
+    // _count: { bids },
+  } = response;
+  document.title = `Gavelbay | ${title}`;
 
-mediaNext.addEventListener("click", slideRight);
-mediaBack.addEventListener("click", slideLeft);
+  // Media
+  for (let i = 0; i < media.length; i++) {
+    const mediaClone = document.importNode(sliderTemplate, true).content;
+    mediaClone.querySelector("#sliderImage").src = media[i];
+    sliderContainer.appendChild(mediaClone);
+  }
+  // Bidders - reverse loop
+  for (let i = bids.length - 1; i >= 0; i--) {
+    const biddersClone = document.importNode(bidderHistoryTemplate, true).content;
+    biddersClone.querySelector("#bidderName").innerText = bids[i].bidderName;
+    biddersClone.querySelector("#bidderAmount").innerText = bids[i].amount;
+    document.querySelector("#bidderHistoryWrap").appendChild(biddersClone);
+  }
+  // Title
+  document.querySelector("#titleWrap").innerText = title.toUpperCase();
+  // Highest bid
+  if (!bids.length) {
+    document.querySelector("#amountWrap").innerText = "No bids";
+    document.querySelector("#bidderHistoryWrap").innerText = "No bidders";
+  } else {
+    document.querySelector("#amountWrap").innerText = bids[bids.length - 1].amount;
+  }
+  // Seller
+  document.querySelector("#sellerWrap").innerText = name;
+  // Ends At
+  document.querySelector("#endsAtWrap").innerText = formatDate(endsAt);
+  // Description
+  document.querySelector("#descriptionWrap").innerText = description;
 
-function slideRight() {
+  arrayLength = media.length - 1;
+}
+buildDetailsPage(id);
+
+// Slide Images eventlistener
+mediaNext.addEventListener("click", () => {
+  slideRight(arrayLength);
+});
+
+mediaBack.addEventListener("click", () => {
+  slideLeft(arrayLength);
+});
+
+// Slide functions
+function slideRight(arrayLength) {
   if (count === arrayLength) {
     count = 0;
     sliderContainer.style.transform = `translateX(-${count * 100}%)`;
@@ -38,7 +95,7 @@ function slideRight() {
   sliderContainer.style.transform = `translateX(-${count * 100}%)`;
 }
 
-function slideLeft() {
+function slideLeft(arrayLength) {
   count--;
   if (count < 0) {
     count = arrayLength;
--- src/js/utils/formatDate.js
+++ src/js/utils/formatDate.js
@@ -1,11 +1,11 @@
 export function formatDate(raw) {
   const date = new Date(raw);
   const currentDate = new Date();
-  if (date === currentDate) {
-    return "Today at" + date.toLocaleTimeString();
+  if (date.toLocaleDateString() === currentDate.toLocaleDateString()) {
+    return "Today at " + date.toLocaleTimeString() + " Local time";
   }
   if (date < currentDate) {
     return "Expired";
   }
-  return "Ends at: " + date.toLocaleDateString();
+  return date.toLocaleDateString() + " At " + date.toLocaleTimeString();
 }
