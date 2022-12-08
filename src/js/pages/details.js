0839de10867b54ebfd63fb1e28d2d1efa4cd227e
WParsec <91538926+WParsec@users.noreply.github.com>
2022-12-08 17:13:36 +0100

    finishes html and css of details page

--- src/js/pages/details.js
+++ src/js/pages/details.js
@@ -13,3 +13,37 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 console.log(id);
+
+// Remember to change name of title of page
+
+// Slider
+const sliderContainer = document.querySelector("#sliderContainer");
+const mediaNext = document.querySelector("#mediaNext");
+const mediaBack = document.querySelector("#mediaBack");
+
+let count = 0;
+let arrayLength = 2;
+
+mediaNext.addEventListener("click", slideRight);
+mediaBack.addEventListener("click", slideLeft);
+
+function slideRight() {
+  if (count === arrayLength) {
+    count = 0;
+    sliderContainer.style.transform = `translateX(-${count * 100}%)`;
+    return;
+  }
+  count++;
+  console.log(count);
+  sliderContainer.style.transform = `translateX(-${count * 100}%)`;
+}
+
+function slideLeft() {
+  count--;
+  if (count < 0) {
+    count = arrayLength;
+    sliderContainer.style.transform = `translateX(-${count * 100}%)`;
+  }
+  console.log(count);
+  sliderContainer.style.transform = `translateX(-${count * 100}%)`;
+}
